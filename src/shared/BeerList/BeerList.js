a0872b4b8fb41c76827c40be0fa1e0c072ad4c6e
Samuele Zanella <samuele@moku.io>
2018-12-07 21:32:42 +0100

    General styling improvements

--- src/shared/BeerList/BeerList.js
+++ src/shared/BeerList/BeerList.js
@@ -3,10 +3,11 @@ import BeerListRow from './BeerListRow/BeerListRow';
 import BeerCount from './BeerCount/BeerCount';
 
 const BeerList = props => (
-  <>
+  <div className='beer-list'>
     <BeerCount beers={props.beers}/>
     {props.beers.map(beer => <BeerListRow key={beer.id} beer={beer}/>)}
-  </>
-  );
+  </div>
+);
+
 
 export default BeerList;
